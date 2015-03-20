c84a06fc9d433ba4f179aef2f70c36e7b54c905f
h3yn3s <hannes1309@gmail.com>
2015-03-20 22:00:22 +0100

    New test

---
+++ Fibonacci/main.js
@@ -0,0 +1,19 @@
+var fiboPoint = 1;
+var fiboCnt = 0;
+var fiboPrevious = 0;
+var fibotemp = 0;
+
+$(document).ready(function(){
+  var btnFibo = $("#btn_fibo");
+  var fiboPoints = $("#fiboPoints");
+  var fiboCnts = $("#fiboCnts");
+
+  $("#btn").click(function(){
+
+  });
+});
+
+function FiboInc(){
+
+  fiboCnts++;
+}
