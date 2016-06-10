35c26d492ca22e7ef58155ac9e94c8d4847838d5
Hannes Jenßen <hannes.jenssen@zlyde.com>
2016-06-10 08:14:53 +0200

    project setup with babel, react, etc

---
+++ Elaine/src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Elaine</h1>
+        <div>Play</div>
+      </div>
+    );
+  }
+}
